f81da6f013bef125d79f9ab233d119bc3e74be25
Andranik Aleksanyan <akaleks3@mts.ru>
2018-08-31 16:35:12 +0300

    New Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { ReaderComponent } from './reader/reader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
